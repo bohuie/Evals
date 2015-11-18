7c7a9c0ae258b25e49c61ed8cd72212bfd6882ee
Bowen Hui <bohuie@gmail.com>
2015-11-18 14:28:29 -0800

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/students' => 'students#index'
+  get '/peer_evals' => 'peer_evals#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
