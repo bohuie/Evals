4c4667f66c07335a31b0621cdcded9c42883e5d8
Bowen Hui <bohuie@gmail.com>
2015-11-18 14:48:16 -0800

    seeded db with simple data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create( first_name: 'Joe', last_name: 'Slacker' )
+Student.create( first_name: 'Jen', last_name: 'Latermore' )
+PeerEval.create( milestone: 'Design', evaluatee: 'Jen Latermore', goals: 1, quality: 2, effort: 3, considerate: 4, knowledge: 5, sharing: 6, strength: 'leadership', weakness: 'time management' )
+PeerEval.create( milestone: 'Design', evaluatee: 'Joe Slacker', goals: 6, quality: 5, effort: 4, considerate: 3, knowledge: 2, sharing: 1, strength: 'smart', weakness: 'people management' )
