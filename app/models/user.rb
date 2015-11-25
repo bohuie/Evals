daafc257e652ed4c5c895932164b7719e1936418
Bowen Hui <bohuie@gmail.com>
2015-11-25 13:23:11 -0800

    basic devise installed, signup works

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  #setup accessible (or protected) attributes for your model
+  # attr_accessible :email, :password, :password_confirmation
 end
