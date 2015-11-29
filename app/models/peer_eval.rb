23015e7e7ac6a9196c8279bb1e321663f5e78af7
Bowen Hui <bohuie@gmail.com>
2015-11-28 21:22:26 -0800

    all form elements are now required before submission

--- app/models/peer_eval.rb
+++ app/models/peer_eval.rb
@@ -1,3 +1,4 @@
 class PeerEval < ActiveRecord::Base
   belongs_to :user 
+  validates :milestone, :goals, :quality, :effort, :considerate, :knowledge, :sharing, :strength, :weakness, presence: true
 end
