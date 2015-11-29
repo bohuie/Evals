f584b7ec57e7592f15d65649e9edf4cb0bb44207
Bowen Hui <bohuie@gmail.com>
2015-11-28 21:32:02 -0800

    peer evals display names rather than ids

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  #setup accessible (or protected) attributes for your model
-  # attr_accessible :email, :password, :password_confirmation
-
   has_many :peer_evals
 
   def fullname
