74d991ac2c0da596bb2358846c3b1eeeb1ef87d2
Bowen Hui <bohuie@gmail.com>
2015-11-30 20:54:33 -0800

    team name when editing user now saves properly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,35 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_admin, only: [:show, :edit, :update]
+
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       redirect_to(:action => 'show', :id => @user.id)
     else
+      flash.now[:error] = @user.errors.full_messages.to_sentence
       render 'edit'
     end
   end
 
   private
   def user_params
-    params.require(:user).permit(:email, :last_name, :first_name)
+    params.require(:user).permit(:email, :last_name, :first_name, :team_id)
+  end
+
+  def authenticate_admin
+    if current_user.admin 
+      @user = User.find(params[:id])
+    else
+      redirect_to welcome_index_path
+    end
   end
 end
