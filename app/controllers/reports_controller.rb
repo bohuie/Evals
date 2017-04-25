388f7e2fd91dd50a9bae84444cc74a0bb6e88890
Rodney Earl <rsearl.90@gmail.com>
2017-04-24 17:16:02 -0700

    Code Cleanup

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,22 +8,22 @@ class ReportsController < ApplicationController
     
     if current_user.admin
       if params[:report] == "student"
-      @report = Report.new
-      @report.user = User.find(params[:user][:id])
-      @report.peer_evals = @report.user.peer_evals.where(:milestone => params[:milestone])
-      @report_type = params[:report]
-    elsif params[:report] == "evals"
-      @report = Report.new
-      @report.user = User.find(params[:user][:id])
-      @report.peer_evals = @report.user.created_peer_evals.where(:milestone => params[:milestone])
-      @report.group_evals = @report.user.group_evals.where(:milestone => params[:milestone])
-      @report_type = params[:report]
-    elsif params[:report] == "groups"
-      @report = Report.new
-      @report.group = Team.find(params[:team][:id])
-      @report.group_evals = @report.group.group_evals.where(:milestone => params[:milestone]).order(:creator_id)
-      @report_type = params[:report]
-    end
+        @report = Report.new
+        @report.user = User.find(params[:user][:id])
+        @report.peer_evals = @report.user.peer_evals.where(:milestone => params[:milestone])
+        @report_type = params[:report]
+      elsif params[:report] == "evals"
+        @report = Report.new
+        @report.user = User.find(params[:user][:id])
+        @report.peer_evals = @report.user.created_peer_evals.where(:milestone => params[:milestone])
+        @report.group_evals = @report.user.group_evals.where(:milestone => params[:milestone])
+        @report_type = params[:report]
+      elsif params[:report] == "groups"
+        @report = Report.new
+        @report.group = Team.find(params[:team][:id])
+        @report.group_evals = @report.group.group_evals.where(:milestone => params[:milestone]).order(:creator_id)
+        @report_type = params[:report]
+      end
     end
     render 'index'
   end
