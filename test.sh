949a50f4714ad576da4cf4b8ebb62c7089ed3b97
Brandon Graham-Knight <brandongk@gmail.com>
2019-11-30 21:47:00 -0800

    Making a test script for local.

--- test.sh
+++ test.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 export TERM=xterm
 
+bundle exec rake
+
 yarn --color=always wait-on http://localhost:3000
 
 yarn --color=always cypress run --project ./spec --browser chrome  --config trashAssetsBeforeRuns=true,baseUrl=http://localhost:3000
